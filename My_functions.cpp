c3d2e3fa5c28dc28a9f95af96c67fee295d5ef2f
adoniskrebs <akrebs@csu.fullerton.edu>
2024-09-15 20:39:52 -0800

    Create My_functions.cpp
    I am getting rid of the folder, as I'm not really sure how to submit this assignment. I think, you're asking us to make another repository with similar files for gradescope or the repository from what you sent. I have to work in a little bit so If i can't finish I'll just have to come in

---
+++ My_functions.cpp
@@ -0,0 +1,22 @@
+/**
+ * Complete this source code file
+ * Yes, this is supposed to be an easy assignment.
+ */
+
+/// YOUR WELCOME
+#include "my_functions.hpp"
+
+
+//
+#include <iostream>
+
+
+//
+int hello_world()
+{
+	std::cout << "Hello, World!" <<
+std::endl;
+	return 23;
+}
+
+
